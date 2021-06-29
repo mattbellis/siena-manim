a9f274efed90ae4679c72023c632f7513350147d
alexisleuci <a21leuc@siena.edu>
2021-06-29 15:11:15 -0400

    Possible intro for all videos

--- text.py
+++ text.py
@@ -1,15 +1,25 @@
 from manim import *
 
+
 class MovingBox(Scene):
+		
 	def construct(self):
-		text = MathTex("0" = "x","+", "5","+","2x")
+		title = Text("Simplifying Equations",color = PURPLE).scale(1.4)
+		self.add(title)
+		self.wait()
+		self.play(FadeOut(title))
+		text = MathTex("0=","x","+5","+2x")
 		self.play(Write(text))
-		framebox1 = SurroundingCircle(text[1],buff=.1)
-		framebox2 = SurroundingCircle(text[5],buff=.1)
-		framebox3 = SurroundingRectangle(text[3],buff=.1)
+		step1 = Text("Step 1 : Box all matching variables").to_corner(UL)
+		self.add(step1)
+		self.wait()
+		framebox1 = SurroundingRectangle(text[1],buff=.1).set_color(YELLOW)
+		framebox2 = SurroundingRectangle(text[3],buff=.1).set_color(YELLOW)
+		framebox3 = SurroundingRectangle(text[2],buff=.1).set_color(BLUE)
 		self.play(Create(framebox1))
 		self.wait()
 		self.play(Create(framebox2))
 		self.wait()
 		self.play(Create(framebox3))
 		self.wait()
+		
