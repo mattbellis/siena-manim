8c35004440a97ee4487a90e263eadd929e04b2a3
alexisleuci <a21leuc@siena.edu>
2021-07-13 16:41:53 -0400

    commit

--- wordproblems.py
+++ wordproblems.py
@@ -1,3 +1,41 @@
 from manim import *
 
-
+class SienaIntro(Scene):
+	def construct(self):
+		sienacollege = Text("Siena College").set_color(GREEN_E).scale(2)
+		triangle = Triangle().set_color(YELLOW_E).scale(1.50)
+		square = Square().set_color(GREEN_E).scale(3)
+		circle = Circle(fill_opacity=5).set_color(YELLOW_E).set_fill().scale(3)
+		circle2 = Circle().set_color(GREEN_E).scale(3)
+		physics = Text("Department of Physics and Astronomy").set_color(YELLOW_E)
+		physics.next_to(sienacollege,DOWN,buff=0).scale(1)	
+		self.play(Create(circle))
+		self.play(Transform(circle, square))
+		self.wait(.5)
+		self.play(Transform(square, triangle))
+		self.wait(.5)
+		self.play(Transform(triangle, circle2))
+		self.wait(.5)
+		
+		triangle_logo= triangle.scale(0.2)
+		circle_logo= circle.scale(0.2)
+		circle_logo.to_corner(DR)
+		triangle_logo.to_corner(DR)
+	#	self.play(FadeOut(circle))
+	#	self.play(FadeOut(square))
+		self.play(Transform(square, sienacollege))
+		self.wait(.5)
+		self.play(Write(physics))
+		self.wait()
+		self.play(FadeOut(physics))
+		square.scale(0.15).next_to(circle,0,buff=0)
+		self.wait()
+		
+		prob1 = Text("If you want to know your position, after some time has passed,\n you take your starting position, add it to your starting\n velocity multiplied by the time that has passed,then add\n one-half the acceleration multiplied by the time that\n has passed squared.").scale(.7)
+		math1 = MathTex("X","(t)","=","X_{0}","+","V_{0}","t","+",".5","a","t^2").next_to(prob1,DOWN)
+		self.play(Write(prob1))
+		self.play(Write(math1),run_time=5) 
+		self.wait(1)
+#		prob1.set_color_by_tex("position,",BLUE)
+#		math1[0].set_color(BLUE)
+		self.wait()
